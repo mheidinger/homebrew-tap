7edf89f28bd201d50f6892bb691e6d53f59f1c3e
Max Heidinger <mail@max-heidinger.de>
2024-11-28 23:15:53 +0100

    feat: add initial preek cask

---
+++ Casks/preek.rb
@@ -0,0 +1,18 @@
+cask "preek" do
+  version "0.5.1"
+  sha256 "82cde6cfb1c4d3647a55999a4acdd8d9c711ebeb3bc10b014736c3cf7866b479"
+
+  url "https://github.com/mheidinger/PReek/releases/download/v#{version}/PReek-#{version}.dmg"
+  name "PReek"
+  desc "GitHub Pull Request Notifications in your Menu Bar"
+  homepage "https://github.com/mheidinger/PReek"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "PReek.app"
+end
